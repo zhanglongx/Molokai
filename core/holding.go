f0b9714f19710418dfd8418f907561d838ca9397
Longxiao Zhang <zhanglongx@gmail.com>
2021-10-20 15:24:57 +0800

    using yaml to store holdings

--- core/holding.go
+++ core/holding.go
@@ -5,64 +5,11 @@
 
 package core
 
-import (
-	"github.com/zhanglongx/Molokai/common"
-	"gorm.io/gorm"
-)
-
-type HoldingStore interface {
-	// Create creates a new holding and persists to database.
-	// Date is in ISO8601 format and with location TZ Asia/Shanghai
-	Create(Symbol common.Symbol, Date string, Cost float64, Reminders string) (*Holding, error)
-
-	// DeleteBySymbol deletes record by Symbol
-	DeleteBySymbol(Symbol common.Symbol) error
-
-	// List returns all of records
-	List() ([]*Holding, error)
-
-	// Save persists all values
-	// The Updated fields is set to current time automatically
-	Save(h *Holding) error
-}
+type Symbol string
 
 type Holding struct {
-	gorm.Model
-
-	Symbol common.Symbol `gorm:"INDEX"`
-	Date   string
+	Symbol Symbol
 	Cost   float64
 
-	Reminders string
-}
-
-var Holdings HoldingStore
-
-type holdings struct {
-	*gorm.DB
-}
-
-func (db *holdings) Create(Symbol common.Symbol, Date string,
-	Cost float64, Reminders string) (*Holding, error) {
-	Holding := &Holding{
-		Symbol:    Symbol,
-		Date:      Date,
-		Cost:      Cost,
-		Reminders: Reminders,
-	}
-
-	return Holding, db.DB.Create(Holding).Error
-}
-
-func (db *holdings) DeleteBySymbol(Symbol common.Symbol) error {
-	return db.Where("symbol = ?", Symbol).Delete(new(Holding)).Error
-}
-
-func (db *holdings) List() ([]*Holding, error) {
-	var holdings []*Holding
-	return holdings, db.Where("TRUE").Find(&holdings).Error
-}
-
-func (db *holdings) Save(h *Holding) error {
-	return db.DB.Save(h).Error
+	Runner interface{}
 }
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
+	"github.com/go-yaml/yaml"
+	"github.com/zhanglongx/Molokai/core"
+)
+
+func main() {
+	if _, err := os.Stat("example.yaml"); os.IsNotExist(err) {
+		log.Fatal("file not exists")
+	}
+
+	buf, err := ioutil.ReadFile("example.yaml")
+	if err != nil {
+		log.Fatal("read example.yaml failed")
+	}
+
+	var holdings core.Holding
+	if err := yaml.Unmarshal(buf, &holdings); err != nil {
+		log.Fatal("parse example.yaml failed")
+	}
+
+	fmt.Println(holdings)
 }
